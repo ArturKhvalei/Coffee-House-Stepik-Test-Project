82bb0284f2ec5dea081da2e7d3e9899efc3323c4
Hei Minami <heimix182@gmail.com>
2022-06-14 22:54:13 +0300

    Added JS Logic

--- js/main.js
+++ js/main.js
@@ -1,45 +1,52 @@
 const userSurname = document.querySelector('#usersurname');
 const userName = document.querySelector('#username');
-
 const goodsElements = document.querySelectorAll('[name="goods"]');
 const countElements = document.querySelectorAll('[name="count"]');
 
 const btn = document.querySelector(".btn");
 const resultElem = document.querySelector(".sum");
 
-const total = [];
+const totalGoods = [];
+const prices = [];
 const selectedGoods = [];
 
 function sum(value, count, i) {
-  total[i] = value * count;
-  resultElem.innerHTML = total.reduce((counter, element) => counter + element, 0);
+  totalGoods[i] = value * count;
+  resultElem.innerHTML = totalGoods.reduce((counter, element) => counter + element, 0) + " р.";
 };
 
 goodsElements.forEach((element, i) => {
-  selectedGoods[i] = parseInt(element.value) || 0;
-  total[i] = 0;
+  prices[i] = parseInt(element.value) || 0;
+  totalGoods[i] = 0;
+  selectedGoods[i] = 0;
 
   element.addEventListener("change", function () {
     if (element.checked) {
       countElements[i].value = 1;
-      sum(selectedGoods[i], 1, i);
+      sum(prices[i], 1, i);
+      selectedGoods[i] = 1;
     } else {
       countElements[i].value = 0;
-      sum(selectedGoods[i], 0, i);
+      sum(prices[i], 0, i);
+      selectedGoods[i] = 0;
     }
   });
 })
 
 countElements.forEach((element, i) => {
   element.addEventListener("change", function () {
-    const subTotal = parseInt(element.value);
-    countElements[i] = subTotal;
-    sum(selectedGoods[i], subTotal, i);
-
-    if (subTotal <= 0) {
-      alert("Пожалуйста, введите значение больше нуля");
-      countElements[i].value = 1;
-      sum(selectedGoods[i], 1, i);
+    const subtotalGoods = parseInt(element.value);
+    countElements[i] = subtotalGoods;
+    if (selectedGoods[i] === 1) {
+      if (Number.isNaN(subtotalGoods) && subtotalGoods >= 1 && Number.isInteger(subtotalGoods)) {
+        sum(prices[i], subtotalGoods, i);
+      } else {
+        countElements[i].value = 1;
+        alert("Пожалуйста, введите целое число больше нуля");
+      }
+    } else {
+      sum(prices[i], 0, i);
+      countElements[i].value = 0;
     }
   });
 });
